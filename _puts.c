51c0782fc17a5dc2dc36ae957e64ec6c1d35533f
Blingblinggiirl <5075@holbertonschool.com>
2022-07-06 12:11:36 -0700

    add puts file

---
+++ _puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - Prints a string
+ * @src: Contains
+ * Return: The length of @src
+ */
+int _puts(char *src)
+{
+	int i;
+
+	for (i = 0; src && scr[i], i++)
+		_putchar(scr[i]);
+
+	return (i);
+}
